52f894f87bd202a3d63310359baa5023fb8cd0d2
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-02-17 14:54:54 +0100

    Solve day 7 part 2

--- javascript/src/day07/index.js
+++ javascript/src/day07/index.js
@@ -3,8 +3,8 @@ import run from "aocrunner"
 const parseInput = (rawInput) => {
   const graph = {}
   rawInput.replace(/ ?bag(s)?\.?/g, '').split('\n').forEach(x => {
-    const [root, tail] = x.split(" contain ")
-    if (tail.includes("no other")) {
+    const [root, tail] = x.split(' contain ')
+    if (tail.includes('no other')) {
       graph[root] = []
       return
     }
@@ -20,7 +20,7 @@ const part1 = (rawInput) => {
   const graph = parseInput(rawInput)
 
   const hasGold = (node) => {
-    if (node == "shiny gold")
+    if (node == 'shiny gold')
       return true
     return graph[node].reduce((acc, [_, child]) => acc || hasGold(child), false)
   }
@@ -28,34 +28,22 @@ const part1 = (rawInput) => {
 }
 
 const part2 = (rawInput) => {
-  const input = parseInput(rawInput)
+  const graph = parseInput(rawInput)
 
-  return
+  const countChildren = (node) => {
+    if (graph[node].length == 0)
+      return 1
+    return graph[node].reduce((acc, [count, child]) => acc + count * countChildren(child), 1)
+  }
+
+  return countChildren('shiny gold') - 1
 }
 
-const part1Input = `light red bags contain 1 bright white bag, 2 muted yellow bags.
-dark orange bags contain 3 bright white bags, 4 muted yellow bags.
-bright white bags contain 1 shiny gold bag.
-muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
-shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
-dark olive bags contain 3 faded blue bags, 4 dotted black bags.
-vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
-faded blue bags contain no other bags.
-dotted black bags contain no other bags.`
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: 4 }
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: "" }
-    ],
     solution: part2,
   },
-  trimTestInputs: true,
-  onlyTests: false,
 })
