6644a50c724e44a6c3cec292233ceb7a03fb9318
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-02-17 08:27:46 +0100

    Solve day 3 part 2

--- javascript/src/day03/index.js
+++ javascript/src/day03/index.js
@@ -2,49 +2,32 @@ import run from "aocrunner"
 
 const parseInput = (rawInput) => rawInput.split('\n')
 
-const part1 = (rawInput) => {
-  const input = parseInput(rawInput)
-
+const trees = (input, dx, dy) => {
   var trees = 0
-  for(var x = 0, y = 0; y < input.length; x+=3, y+=1) {
+  for(var x = 0, y = 0; y < input.length; x+=dx, y+=dy) {
     if (input[y][x%input[0].length] == "#")
       trees++
   }
-
   return trees
+}
+
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput)
+
+  return trees(input, 3, 1)
 }
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput)
 
-  return
+  return trees(input, 1, 1) * trees(input, 3, 1) * trees(input, 5, 1) * trees(input, 7, 1) * trees(input, 1, 2)
 }
 
 run({
   part1: {
-    tests: [
-      { input: `
-      ..##.......
-      #...#...#..
-      .#....#..#.
-      ..#.#...#.#
-      .#...##..#.
-      ..#.##.....
-      .#.#.#....#
-      .#........#
-      #.##...#...
-      #...##....#
-      .#..#...#.#
-      `, expected: 7 }
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: ``, expected: "" }
-    ],
     solution: part2,
   },
-  trimTestInputs: true,
-  onlyTests: false,
 })
