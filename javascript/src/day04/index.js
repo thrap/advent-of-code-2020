0ae7afbb2c97593d38d99e05efb2b5877a6eb089
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-02-17 09:07:56 +0100

    Solve day 4 part 2

--- javascript/src/day04/index.js
+++ javascript/src/day04/index.js
@@ -1,12 +1,12 @@
-import run from "aocrunner"
+import run from 'aocrunner'
 
-const parseInput = (rawInput) => rawInput
+const parseInput = (rawInput) => rawInput.split('\n\n').map(s => s.replace(/\n/g, ' ').split(' ').map(line => line.split(':')))
 
 const part1 = (rawInput) => {
-  const input = parseInput(rawInput).split("\n\n").map(s => s.replace(/\n/g, ' ').split(" ").map(line => line.split(":")[0]))
+  const input = parseInput(rawInput)
   const isValid = (fields) => {
     const FIELDS =  ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-    return FIELDS.every(field => fields.includes(field))
+    return FIELDS.every(field => fields.map(x => x[0]).includes(field))
   }
 
   return input.filter(line => isValid(line)).length
@@ -14,37 +14,35 @@ const part1 = (rawInput) => {
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput)
+  const isValid = (pass) => {
+    const fields = pass.reduce((acc, [a, b]) => {
+      acc[a] = b; return acc
+    }, {})
+    const FIELDS =  [
+      ['byr', str => str >= 1920 && str <= 2002],
+      ['iyr', str => str >= 2010 && str <= 2020],
+      ['eyr', str => str >= 2020 && str <= 2030],
+      ['hgt', str => {
+        const [_, size, metric] =  str.split(/(\d+)/)
+        if (metric == 'cm')
+        return size >= 150 && size <= 193
+        return metric == 'in' && size >= 59 && size <= 76
+      }],
+      ['hcl', str => /^#[0-9a-f]{6}$/.test(str)],
+      ['ecl', str => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(str)],
+      ['pid', str => /^[0-9]{9}$/.test(str)],
+    ]
+    return FIELDS.every(([field, validator]) => validator(fields[field] || ''))
+  }
 
-  return
+  return input.filter(line => isValid(line)).length
 }
 
-const part1Input = `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
-byr:1937 iyr:2017 cid:147 hgt:183cm
-
-iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
-hcl:#cfa07d byr:1929
-
-hcl:#ae17e1 iyr:2013
-eyr:2024
-ecl:brn pid:760753108 byr:1931
-hgt:179cm
-
-hcl:#cfa07d eyr:2025 pid:166559648
-iyr:2011 ecl:brn hgt:59in`
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: 2 }
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: "" }
-    ],
     solution: part2,
   },
-  trimTestInputs: true,
-  onlyTests: false,
 })
