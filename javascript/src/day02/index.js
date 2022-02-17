6ae4d91ee1856347150747ad928eb2b3da51f1bb
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-02-17 08:08:46 +0100

    Solve day 2 part 2

--- javascript/src/day02/index.js
+++ javascript/src/day02/index.js
@@ -18,29 +18,22 @@ const part1 = (rawInput) => {
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput)
+  const isValid = (line) => {
+    const [policy, str] = line.split(": ")
+    const [repeats, char] = policy.split(" ")
+    const [min, max] = repeats.split("-").map(x => parseInt(x))
 
-  return
+    return str[min-1] == char ^ str[max-1] == char
+  }
+
+  return input.filter(line => isValid(line)).length
 }
 
 run({
   part1: {
-    tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
-    ],
     solution: part2,
   },
-  trimTestInputs: true,
-  onlyTests: false,
 })
