29087abeb1fb4048a607be6204e661e72cfaaea4
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-02-18 16:11:25 +0100

    Solve day 14 part 2

--- javascript/src/day14/index.js
+++ javascript/src/day14/index.js
@@ -2,15 +2,16 @@ import run from "aocrunner"
 
 const parseInput = (rawInput) => rawInput.split('\n').map(l => l.split(' = '))
 
+const pad = (str) => {
+  while (str.length != 36) {
+    str = "0"+str
+  }
+  return str
+}
+
 const part1 = (rawInput) => {
   const input = parseInput(rawInput)
 
-  const pad = (str) => {
-    while (str.length != mask.length) {
-      str = "0"+str
-    }
-    return str
-  }
   const transform = (mask, str) => {
     var newStr = ""
     for(var i = 0; i < mask.length; i++) {
@@ -39,27 +40,50 @@ const part1 = (rawInput) => {
 const part2 = (rawInput) => {
   const input = parseInput(rawInput)
 
-  return
+  const transform = (mask, str) => {
+    var newStr = ""
+    for(var i = 0; i < mask.length; i++) {
+      newStr += mask[i] == '0' ? str[i] : mask[i]
+    }
+    return newStr
+  }
+
+  const memory = {}
+  const expand = (str, val, front = '') => {
+    if (str.length == 0) {
+      memory[front] = val
+      return
+    }
+    const c = str[0]
+    const tail = str.substr(1)
+    if (c == 'X') {
+      expand(tail, val, front + '0')
+      expand(tail, val, front + '1')
+    } else {
+      expand(tail, val, front + c)
+    }
+  }
+
+  var mask = ''
+  input.forEach(([l, r]) => {
+    if (l == 'mask') {
+      mask = r
+    } else {
+      const adr = +l.match(/mem\[(\d+)\]/)[1]
+      const val = +r
+      const binary = transform(mask, pad(adr.toString(2)))
+      expand(binary, val)
+    }
+  });
+
+  return Object.values(memory).reduce((acc, x) => acc + x)
 }
 
-const part1Input = `mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-mem[8] = 11
-mem[7] = 101
-mem[8] = 0`
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: 165 }
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: "" }
-    ],
     solution: part2,
   },
-  trimTestInputs: true,
-  onlyTests: false,
 })
