8c90f34756ae6f20f6801ed06b1ac0ca3b82e028
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-02-18 11:39:42 +0100

    Solve day 12 part 1

---
+++ javascript/src/day12/index.js
@@ -0,0 +1,45 @@
+import run from "aocrunner"
+
+const parseInput = (rawInput) => rawInput.split('\n')
+
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput).map(l => [l[0], +l.substr(1)])
+  const dirs = [[-1,0],[0,1],[1,0],[0,-1]]
+  var dir = 1
+  const pos = [0,0]
+
+  input.forEach(([action, val]) => {
+    if (action == 'N') {
+      pos[0] -= val
+    } else if (action == 'S') {
+      pos[0] += val
+    } else if (action == 'E') {
+      pos[1] += val
+    } else if (action == 'W') {
+      pos[1] -= val
+    } else if (action == 'R') {
+      dir += val/90
+    } else if (action == 'L') {
+      dir -= val/90
+    } else if (action == 'F') {
+      const d = dirs[(4 + dir % 4) % 4]
+      pos[0] += d[0] * val
+      pos[1] += d[1] * val
+    }
+  });
+
+  return Math.abs(pos[0]) + Math.abs(pos[1])
+}
+
+const part2 = (rawInput) => {
+  return
+}
+
+run({
+  part1: {
+    solution: part1,
+  },
+  part2: {
+    solution: part2,
+  },
+})
