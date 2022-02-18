da8c4a83e67d80ed8c8482498d6cc9a5134c7cf0
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-02-18 10:50:03 +0100

    Solve day 11 part 2

--- javascript/src/day11/index.js
+++ javascript/src/day11/index.js
@@ -1,85 +1,52 @@
 import run from "aocrunner"
 
-const parseInput = (rawInput) => rawInput.split('\n').map(l => l.split(''))
+const parse = (input) => input.split('\n').map(l => l.split(''))
 
-const part1 = (rawInput) => {
-  const dirs = [[1,0],[-1,0],[0,1],[0,-1],[1,1],[1,-1],[-1,1],[-1,-1]]
-  var state = parseInput(rawInput)
-
-  const stateStr = (board) => board.map(x => x.join('')).join('\n')
-
-  const printBoard = (board) => {
-    console.log(stateStr(board))
-    console.log();
-  }
-
-  const step = (state) => {
-    const around = (i, j) => {
-      const count = [0, 0]
-      dirs.forEach(([di, dj]) => {
-        const c = (state[i+di]||[])[j+dj]
-        if (c == 'L')
-          count[0]++
-        if (c == '#')
-          count[1]++
-      })
-      return count
+const dirs = [[1,0],[-1,0],[0,1],[0,-1],[1,1],[1,-1],[-1,1],[-1,-1]]
+const occupied = (state, i, j, part2) => {
+  var count = 0
+  dirs.forEach(([di, dj]) => {
+    var c = (state[i+di]||[])[j+dj]
+    for(var l = 2; part2 && c == '.'; l++) {
+      c = (state[i+l*di]||[])[j+l*dj]
     }
+    if (c == '#')
+      count++
+  })
+  return count
+}
 
-    var newState = Array(state.length).fill(true).map(_ => Array(state[0].length).fill('X'))
-    for(var i = 0; i < state.length; i++) {
-      for (var j = 0; j < state[0].length; j++) {
-        const u = around(i, j)
-        if (state[i][j] == 'L' && u[1] == 0) {
-          newState[i][j] = '#'
-        } else if (state[i][j] == '#' && u[1] >= 4) {
-          newState[i][j] = 'L'
-        } else {
-          newState[i][j] = state[i][j]
-        }
+const stateStr = (board) => board.map(x => x.join('')).join('\n')
+
+const step = (state, part2, prev) => {
+  if (prev == stateStr(state)) {
+    return prev.replace(/\n/g,'').split('').filter(s => s == '#').length
+  }
+  var newState = Array(state.length).fill(true).map(_ => Array(state[0].length).fill('X'))
+  for(var i = 0; i < state.length; i++) {
+    for (var j = 0; j < state[0].length; j++) {
+      const u = occupied(state, i, j, part2)
+      if (state[i][j] == 'L' && u == 0) {
+        newState[i][j] = '#'
+      } else if (state[i][j] == '#' && u >= 4 + (part2 ? 1 : 0)) {
+        newState[i][j] = 'L'
+      } else {
+        newState[i][j] = state[i][j]
       }
     }
-    return newState
   }
-
-  var prev
-  for(var steps = 0; prev != stateStr(state); steps++) {
-    prev = stateStr(state)
-    state = step(state)
-  }
-  return prev.replace(/\n/g,'').split('').filter(s => s == '#').length
+  return step(newState, part2, stateStr(state))
 }
 
-const part2 = (rawInput) => {
-  const input = parseInput(rawInput)
+const part1 = (input) => step(parse(input))
 
-  return
-}
+const part2 = (input) => step(parse(input), true)
 
-const part1Input = `L.LL.LL.LL
-LLLLLLL.LL
-L.L.L..L..
-LLLL.LL.LL
-L.LL.LL.LL
-L.LLLLL.LL
-..L.L.....
-LLLLLLLLLL
-L.LLLLLL.L
-L.LLLLL.LL`
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: 37 }
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: "" }
-    ],
     solution: part2,
   },
-  trimTestInputs: true,
-  onlyTests: false,
 })
