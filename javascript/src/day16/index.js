df77c3629a3fc98f2628d7cbaa0ecdfc60f8ce3b
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-02-19 21:43:07 +0100

    Solve day 16 part 2

--- javascript/src/day16/index.js
+++ javascript/src/day16/index.js
@@ -2,7 +2,7 @@ import run from "aocrunner"
 
 const parseInput = (rawInput) => {
   const [a,b,c] = rawInput.split('\n\n').map(group => group.split('\n'))
-  return [a, b.slice(1), c.slice(1).map(l => l.split(',').map(x => +x))]
+  return [a, b.slice(1)[0].split(',').map(x => +x), c.slice(1).map(l => l.split(',').map(x => +x))]
 }
 
 const part1 = (rawInput) => {
@@ -19,37 +19,58 @@ const part1 = (rawInput) => {
 }
 
 const part2 = (rawInput) => {
-  const input = parseInput(rawInput)
+  const [header, ticket, nearby] = parseInput(rawInput)
+  const fields = header.map(x => [x.split(': ')[0], x.split(': ')[1].split(' or ').map(range => range.split('-').map(x => +x))])
 
-  return
+  const candidates = {}
+  fields.forEach(([field, range]) => {
+    range.forEach(([min, max]) => {
+      for(var x = min; x <= max; x++) {
+        if (!candidates[x]) {
+          candidates[x] = [field]
+        } else {
+          candidates[x].push(field)
+        }
+      }
+    })
+  });
+
+  const valid = nearby.filter(x => x.every(n => candidates[n]))
+
+  var possible = []
+  for(var pos = 0; pos < valid[0].length; pos++) {
+    var candidate = candidates[valid[0][pos]]
+    for(var i = 0; i < valid.length; i++) {
+      var b = new Set(candidates[valid[i][pos]])
+      candidate = candidate.filter(x => b.has(x))
+    }
+    possible.push(candidate)
+  }
+
+  const correct = {}
+
+  for (var i = 0; i < 100; i++) {
+    for(var pos = 0; pos < valid[0].length; pos++) {
+      const asd = possible[pos].filter(x => !Object.keys(correct).includes(x))
+      if (asd.length == 1) {
+        correct[asd[0]] = pos
+      }
+    }
+  }
+
+  var product = 1
+  Object.keys(correct).filter(field => field.substring(0, 9) == 'departure').forEach(field => {
+    product *= ticket[correct[field]]
+  })
+
+  return product
 }
 
-const part1Input = `class: 1-3 or 5-7
-row: 6-11 or 33-44
-seat: 13-40 or 45-50
-
-your ticket:
-7,1,14
-
-nearby tickets:
-7,3,47
-40,4,50
-55,2,20
-38,6,12`
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: "" }
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: "" }
-    ],
     solution: part2,
   },
-  trimTestInputs: true,
-  onlyTests: false,
 })
