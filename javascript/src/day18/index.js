cb4898df29da68c3001640af56e8adc99e7351bc
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-02-20 12:08:41 +0100

    Solve day 18 part 2

--- javascript/src/day18/index.js
+++ javascript/src/day18/index.js
@@ -2,31 +2,41 @@ import run from "aocrunner"
 
 const parseInput = (rawInput) => rawInput.replace(/ /g, '').split('\n')
 
-const part1 = (rawInput) => {
-  const input = parseInput(rawInput)
-  const evalu = (s) => {
-    if (/^\d+$/.test(s))
-      return +s
+const evalu = (s) => {
+  if (/^\d+$/.test(s))
+    return +s
 
-    return evalu(s.replace(/(\d+)([*+])(\d+)/, (_, a, op, b) =>
-      op == '*' ? a * b : +a + (+b)
+  return evalu(s.replace(/(\d+)([*+])(\d+)/, (_, a, op, b) =>
+    op == '*' ? a * b : +a + (+b)
+  ))
+}
+
+const evalu2 = (s) => {
+  if (s.includes('+')) {
+    return evalu2(s.replace(/(\d+)[+](\d+)/, (_, a, b) =>
+      +a + (+b)
     ))
   }
-  const brackets = str => {
-    const repl = str.replace(/\([^()]+\)/g, (a) => evalu(a.substring(1, a.length-1)))
+  return evalu(s)
+}
 
-    if (repl == str)
-      return evalu(str)
-    else
-      return brackets(repl)
-  }
-  return input.map(l => brackets(l)).reduce((acc, x) => acc + x)
+const brackets = (str, evalu) => {
+  const repl = str.replace(/\([^()]+\)/g, (a) => evalu(a.substring(1, a.length-1)))
+
+  if (repl == str)
+    return evalu(str)
+  else
+    return brackets(repl, evalu)
+}
+
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput)
+  return input.map(l => brackets(l, evalu)).reduce((acc, x) => acc + x)
 }
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput)
-
-  return
+  return input.map(l => brackets(l, evalu2)).reduce((acc, x) => acc + x)
 }
 
 run({
@@ -41,6 +51,11 @@ run({
   },
   part2: {
     tests: [
+      { input: '1 + (2 * 3) + (4 * (5 + 6))', expected: 51 },
+      { input: '2 * 3 + (4 * 5)', expected: 46 },
+      { input: '5 + (8 * 3 + 9 + 3 * 4 * 3)', expected: 1445 },
+      { input: '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))', expected: 669060 },
+      { input: '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2', expected: 23340 },
     ],
     solution: part2,
   },
