c5459810d7b6ea82da4dd02e5ec3ceb27cdf3e07
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-01 14:42:02 +0100

    Solve day 19 part 2

--- javascript/src/day19/index.js
+++ javascript/src/day19/index.js
@@ -13,8 +13,6 @@ const parseInput = (rawInput) => {
 const part1 = (rawInput) => {
   const [rules, messages] = parseInput(rawInput)
 
-  console.log(rules);
-
   const replace = (k, v) => {
     Object.keys(rules).forEach(key => {
       rules[key] = rules[key].split(' ').map(c => c == k ? v : c).join(' ')
@@ -26,7 +24,6 @@ const part1 = (rawInput) => {
       const rule = rules[key]
       if (!/\d/.test(rule) && key != '0') {
         replace(key, '('+rules[key].replace(/ /g, '')+')');
-        delete rules[key]
       }
     })
   }
@@ -37,40 +34,47 @@ const part1 = (rawInput) => {
 }
 
 const part2 = (rawInput) => {
-  const input = parseInput(rawInput)
+  const [rules, messages] = parseInput(rawInput)
 
-  return
+  const done = { '0' : true}
+  const replace = (k, v) => {
+    Object.keys(rules).forEach(key => {
+      rules[key] = rules[key].split(' ').map(c => c == k ? v : c).join(' ')
+    })
+  }
+
+
+  for(var i = 0; i < 10; i++) {
+    Object.keys(rules).forEach(key => {
+      const rule = rules[key]
+      if (!/\d/.test(rule) && !done[key]) {
+        rules[key] = rules[key].replace(/ /g, '')
+        if (key == '8') {
+          rules[8] = '('+rules[8]+')+'
+        }
+        if (key == '11') {
+          const a = '('+rules[42]+')';
+          const b = '('+rules[31]+')'
+
+          const c = "("+a+b+")|("+a+a+b+b+")|("+a+a+a+b+b+b+")|("+a+a+a+a+b+b+b+b+")"
+          rules[key] = c
+        }
+        replace(key, '('+rules[key]+')');
+        done[key] = true
+      }
+    })
+  }
+
+  const regex = new RegExp('^'+rules[0].replace(/ /g, '')+'$');
+
+  return messages.filter(m => regex.test(m)).length
 }
 
-const part1Input = `0: 4 1 5
-1: 2 3 | 3 2
-2: 7
-7: 8 | 5 5
-3: 4 5 | 5 4
-4: "a"
-5: 6
-6: "b"
-8: 4 4
-
-ababbb
-bababa
-abbbab
-aaabbb
-aaaabbb`
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: "" }
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: "" }
-    ],
     solution: part2,
   },
-  trimTestInputs: true,
-  onlyTests: false,
 })
