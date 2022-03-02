c3a447b304b19fcf0553709c7b742b3a7644c9c8
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-02 12:55:21 +0100

    Solve day 21 part 2

--- javascript/src/day21/index.js
+++ javascript/src/day21/index.js
@@ -5,82 +5,61 @@ const parseInput = (rawInput) => rawInput.split('\n').map(l => {
   return [split[0].split(" "), split[1].substring(0,split[1].length-1).split(', ')]
 })
 
-const part1 = (rawInput) => {
-  const input = parseInput(rawInput)
+const intersect = (a, b) => new Set([...a].filter(i => b.has(i)));
+
+const getMapping = input => {
   const candidates = {}
   input.forEach(([ingredients, allergens]) => {
     allergens.forEach(allergen => {
       if (!candidates[allergen])
-        candidates[allergen] = []
-      candidates[allergen].push(new Set(ingredients))
+        candidates[allergen] = new Set(ingredients)
+      candidates[allergen] = intersect(new Set(ingredients), candidates[allergen])
     })
   })
 
-  const intersect = (a, b) => new Set([...a].filter(i => b.has(i)));
-
-  const allergens = {}
-  const mapping = {}
-  // kan gjøre dette i første foreach
-  Object.keys(candidates).forEach(allergen => {
-    const set = candidates[allergen].reduce((acc, set) => intersect(acc, set))
-    allergens[allergen] = set
-  })
-
-  console.log(allergens);
-
   const removeIngredient = ingredient => {
-    Object.keys(allergens).forEach(allergen => {
-      allergens[allergen].delete(ingredient)
+    Object.keys(candidates).forEach(allergen => {
+      candidates[allergen].delete(ingredient)
     })
   }
 
+  const mapping = {}
   for(var i = 0; i < 100; i++) {
-    Object.keys(allergens).forEach(allergen => {
-      const set = allergens[allergen]
+    Object.keys(candidates).forEach(allergen => {
+      const set = candidates[allergen]
       if (set.size == 1) {
         const ingredient = [...set][0];
         removeIngredient(ingredient)
-        delete allergens[allergen]
+        delete candidates[allergen]
         mapping[allergen] = ingredient
-        console.log(allergen, ingredient);
       }
     })
   }
 
+  return mapping
+}
+
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput)
+  const mapping = getMapping(input)
+
   const dangerous = new Set(Object.values(mapping))
 
   return input.map(x => x[0].filter(i => !dangerous.has(i))).flat().length
-  console.log(dangerous);
-
-  console.log(allergens);
-
-  return
 }
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput)
+  const mapping = getMapping(input)
 
-  return
+  return Object.keys(mapping).sort().map(allergen => mapping[allergen]).join(',');
 }
 
-const part1Input = `mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
-trh fvjkl sbzzf mxmxvkd (contains dairy)
-sqjhc fvjkl (contains soy)
-sqjhc mxmxvkd sbzzf (contains fish)`
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: "" }
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: "" }
-    ],
     solution: part2,
   },
-  trimTestInputs: true,
-  onlyTests: false,
 })
