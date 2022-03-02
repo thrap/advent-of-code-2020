489f21edc2562d86ffea7ee4874e7394e98ca70b
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-02 19:11:49 +0100

    Solve day 24 part 2

--- javascript/src/day24/index.js
+++ javascript/src/day24/index.js
@@ -24,7 +24,7 @@ const parseLine = ([a, b, ...rest]) => {
   }
 }
 
-const part1 = (rawInput) => {
+const getFlip = rawInput => {
   const input = parseInput(rawInput)
 
   const flip = {}
@@ -35,52 +35,59 @@ const part1 = (rawInput) => {
       x += dx
       y += dy
     })
-    flip[x+' '+y] = !flip[x+' '+y]
+    flip[[x,y]] = !flip[[x,y]]
   })
+  return flip
+}
+
+const part1 = (rawInput) => {
+  const flip = getFlip(rawInput)
 
   return Object.values(flip).filter(x => x).length
 }
 
 const part2 = (rawInput) => {
-  const input = parseInput(rawInput)
+  const flip = getFlip(rawInput)
 
-  return
+  const getNeighbours = (x, y) => Object.values(dirs).map(([dx, dy]) => [x + dx, y + dy])
+
+  const doStep = flip => {
+    const newFlip = {}
+    Object.keys(flip).map(s => s.split(',').map(s => +s)).forEach(([x,y]) => {
+      const neighbours = getNeighbours(x, y)
+
+      neighbours.concat([[x,y]]).forEach(([nx, ny]) => {
+        const flipped = getNeighbours(nx, ny).filter(x => flip[x]).length
+
+        if (flip[[nx, ny]]) {
+          if (flipped != 0 && flipped <= 2) {
+            newFlip[[nx, ny]] = true
+          }
+        } else {
+          if (flipped == 2) {
+            newFlip[[nx, ny]] = true
+          }
+        }
+      })
+    })
+    return newFlip
+  }
+
+  var asd = flip
+  for(var step = 1; step <= 100; step++) {
+    asd = doStep(asd)
+    if (step % 10 == 0)
+      console.log('step ' + step + '/100 :', Object.keys(asd).length);
+  }
+
+  return Object.keys(asd).length
 }
 
-const part1Input = `sesenwnenenewseeswwswswwnenewsewsw
-neeenesenwnwwswnenewnwwsewnenwseswesw
-seswneswswsenwwnwse
-nwnwneseeswswnenewneswwnewseswneseene
-swweswneswnenwsewnwneneseenw
-eesenwseswswnenwswnwnwsewwnwsene
-sewnenenenesenwsewnenwwwse
-wenwwweseeeweswwwnwwe
-wsweesenenewnwwnwsenewsenwwsesesenwne
-neeswseenwwswnwswswnw
-nenwswwsewswnenenewsenwsenwnesesenew
-enewnwewneswsewnwswenweswnenwsenwsw
-sweneswneswneneenwnewenewwneswswnese
-swwesenesewenwneswnwwneseswwne
-enesenwswwswneneswsenwnewswseenwsese
-wnwnesenesenenwwnenwsewesewsesesew
-nenewswnwewswnenesenwnesewesw
-eneswnwswnwsenenwnwnwwseeswneewsenese
-neswnwewnwnwseenwseesewsenwsweewe
-wseweeenwnesenwwwswnew`
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: 10 }
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: "" }
-    ],
     solution: part2,
   },
-  trimTestInputs: true,
-  onlyTests: false,
 })
