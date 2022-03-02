09ff319c3480cdd267322e1cf9c26e174101906c
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-02 14:20:48 +0100

    Solve day 22 part 2

--- javascript/src/day22/index.js
+++ javascript/src/day22/index.js
@@ -21,38 +21,48 @@ const part1 = (rawInput) => {
 }
 
 const part2 = (rawInput) => {
-  const input = parseInput(rawInput)
+  const [deckA, deckB] = parseInput(rawInput)
 
-  return
+  var concat = []
+  const round = (A, B, acc) => {
+    concat = A.concat(B)
+
+    if (!A.length) return 2
+    if (!B.length) return 1
+
+    const str1 = "A: "+A
+    const str2 = "B: "+B
+    if (acc[str1] || acc[str2]) return 1
+
+    acc[str1] = true
+    acc[str2] = true
+
+    const a = A.shift()
+    const b = B.shift()
+
+    const recur = a <= A.length && b <= B.length
+    const winner = recur ? game(A.slice(0, a), B.slice(0, b)) : (a > b ? 1 : 2)
+    if (winner == 1) {
+      return round(A.concat([a,b]), B, acc)
+    } else {
+      return round(A, B.concat([b,a]), acc)
+    }
+  }
+
+  const game = (A, B) => {
+    return round(A, B, {})
+  }
+
+  game(deckA, deckB)
+
+  return concat.reverse().reduce((acc, x, i) => acc + x*(i+1), 0)
 }
 
-const part1Input = `Player 1:
-9
-2
-6
-3
-1
-
-Player 2:
-5
-8
-4
-7
-10`
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: "" }
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: "" }
-    ],
     solution: part2,
   },
-  trimTestInputs: true,
-  onlyTests: false,
 })
